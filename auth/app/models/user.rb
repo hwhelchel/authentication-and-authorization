2fcd13e6803037963345a5b46e62c3a9d7f56a44
Harry Whelchel <harrywhelchel@gmail.com>
2014-02-23 09:35:07 -0800

    Unit testing in process

--- auth/app/models/user.rb
+++ auth/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
   validates_presence_of :email
-  # validates_format_of :email, with: EMAIL_REGEX, on: :create
+  validates_format_of :email, with: EMAIL_REGEX, on: :create
 
   include BCrypt
 
